0e8bae378b0d186a95569e180ed277efcd72ce6f
Zaxyn <ruan.pablo2002@outlook.com.br>
2022-10-20 01:51:22 -0300

    Controle de acessos v1

---
+++ src/main/java/com/RuanPablo1/Gerenciamento/GerenciamentoApplication.java
@@ -0,0 +1,12 @@
+package com.RuanPablo1.Gerenciamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoApplication.class, args);
+	}
+}
